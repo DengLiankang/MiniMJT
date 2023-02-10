75caceb9d6b3faf4b64698a160ba0dc3f87ebe23
Deng Liankang <liankang.deng@qq.com>
2023-02-10 13:33:00 +0800

    add esp32-pico-d4 configuration，update panel pin setting

--- lib/TFT_eSPI/User_Setup.h
+++ lib/TFT_eSPI/User_Setup.h
@@ -192,8 +192,8 @@
 #define TFT_MISO 19
 #define TFT_MOSI 23
 #define TFT_SCLK 18
-// #define TFT_CS   -1 // Not connected
-#define TFT_CS   15  // Chip select control pin
+#define TFT_CS   -1 // Not connected
+// #define TFT_CS   15  // Chip select control pin
 #define TFT_DC    2  // Data Command control pin
 #define TFT_RST   4  // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
@@ -311,7 +311,7 @@
 // #define SPI_FREQUENCY  27000000
 // #define SPI_FREQUENCY  40000000
 // #define SPI_FREQUENCY  55000000 // STM32 SPI1 only (SPI2 maximum is 27MHz)
-#define SPI_FREQUENCY  80000000
+#define SPI_FREQUENCY  60000000
 
 // Optional reduced SPI frequency for reading TFT
 #define SPI_READ_FREQUENCY  20000000
